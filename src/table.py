9f572d658c9da1a65e15d1e46d45cf6b8dacaeca
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2019-01-17 14:48:58 +0100

    working on table

--- src/table.py
+++ src/table.py
@@ -1,7 +1,6 @@
 import math
 import numpy as num
-from pyrocko.guts import Object, String, Unicode, List, Int, SObject, Any, \
-    Defer
+from pyrocko.guts import Object, String, Unicode, List, Int, SObject, Any
 from pyrocko import orthodrome as od
 from pyrocko.util import num_full
 
@@ -30,14 +29,39 @@ class DType(SObject):
 
 class SubHeader(Object):
     name = String.T()
-    label = Unicode.T(optional=True)
     unit = Unicode.T(optional=True)
-    dtype = DType.T(default=num.dtype('float64'), optional=True)
-    default = Any.T(default=0.0)  # default_value
+    default = Any.T(optional=True)
+    label = Unicode.T(optional=True)
+
+    def __init__(self, name, unit=None, default=None, label=None, **kwargs):
+        Object.__init__(
+            self, name=name, unit=unit, default=default, label=label, **kwargs)
+
+    def get_caption(self):
+        s = self.label or self.name
+        if self.unit:
+            s += ' [%s]' % self.unit
+
+        return s
 
 
 class Header(SubHeader):
     sub_headers = List.T(SubHeader.T())
+    dtype = DType.T(default=num.dtype('float64'), optional=True)
+
+    def __init__(
+            self, name,
+            unit=None,
+            sub_headers=[],
+            dtype=None,
+            default=None,
+            label=None):
+
+        sub_headers = [anything_to_sub_header(sh) for sh in sub_headers]
+
+        kwargs = dict(sub_headers=sub_headers, dtype=dtype)
+
+        SubHeader.__init__(self, name, unit, default, label, **kwargs)
 
     def get_ncols(self):
         return max(1, len(self.sub_headers))
@@ -48,6 +72,28 @@ class Header(SubHeader):
             return num_full((nrows,), val, dtype=self.dtype)
         else:
             return num_full((nrows, self.get_ncols()), val, dtype=self.dtype)
+
+
+def anything_to_header(args):
+    if isinstance(args, Header):
+        return args
+    elif isinstance(args, str):
+        return Header(name=args)
+    elif isinstance(args, tuple):
+        return Header(*args)
+    else:
+        raise ValueError('argument of type Header, str or tuple expected')
+
+
+def anything_to_sub_header(args):
+    if isinstance(args, SubHeader):
+        return args
+    elif isinstance(args, str):
+        return SubHeader(name=args)
+    elif isinstance(args, tuple):
+        return SubHeader(*args)
+    else:
+        raise ValueError('argument of type SubHeader, str or tuple expected')
 
 
 class Description(Object):
@@ -63,6 +109,10 @@ class Description(Object):
             headers=table._headers,
             nrows=table.get_nrows(),
             ncols=table.get_ncols())
+
+
+class NoSuchRecipe(Exception):
+    pass
 
 
 class Table(object):
@@ -117,6 +167,8 @@ class Table(object):
         return len(self._arrays)
 
     def add_col(self, header, array=None):
+        header = anything_to_header(header)
+
         nrows_current = self.get_nrows()
         if array is None:
             array = header.default_array(nrows_current)
@@ -161,6 +213,7 @@ class Table(object):
 
     def add_rows(self, arrays):
         assert self.get_ncols() == len(arrays)
+        arrays = [num.asarray(arr) for arr in arrays]
 
         nrows_add = nrows(arrays[0])
         nrows_current = self.get_nrows()
@@ -194,33 +247,88 @@ class Table(object):
         else:
             recipe = self.get_recipe_for_col(name)
             recipe._update_col(self, name)
+
+            print('mm', mask)
             return recipe.get_table().get_col(name, mask)
+
+    def get_header(self, name):
+        if name in self._cols:
+            iarr, icol = self._cols[name]
+            if icol is None:
+                return self._headers[iarr]
+            else:
+                return self._headers[iarr].sub_headers[icol]
+        else:
+            recipe = self.get_recipe_for_col(name)
+            return recipe.get_header(name)
 
     def has_col(self, name):
         return name in self._cols or \
-            any(name in rec.get_col_names() for rec in self.recipes)
+            any(rec.has_col(name) for rec in self.recipes)
 
-    def get_col_names(self):
+    def get_col_names(self, sub_headers=True):
         names = []
         for h in self._headers:
             names.append(h.name)
-            for sh in h.sub_headers:
-                names.append(sh.name)
+            if sub_headers:
+                for sh in h.sub_headers:
+                    names.append(sh.name)
 
-        for recipe in self.recipies:
+        for recipe in self.recipes:
             names.extend(recipe.get_col_names())
 
         return names
 
     def get_recipe_for_col(self, name):
         for recipe in self.recipes:
+            print(recipe, recipe.has_col(name), name)
             if recipe.has_col(name):
+                print('xx')
                 return recipe
+                print('yyy')
 
-    def __str__(self):
+        print('yy')
+        raise NoSuchRecipe(name)
+
+    def get_description(self):
         d = Description(self)
         d.validate()
         return str(Description(self))
+
+    def __str__(self):
+        scols = []
+        formats = {
+            num.dtype('float64'): '%e'}
+
+        for name in self.get_col_names(sub_headers=False):
+            array = self.get_col(name)
+            header = self.get_header(name)
+            fmt = formats.get(array.dtype, '%s')
+            if array.ndim == 1:
+                scol = [header.get_caption(), '']
+                for val in array:
+                    scol.append(fmt % val)
+
+                scols.append(scol)
+            else:
+                for icol in range(ncols(array)):
+                    sub_header = header.sub_headers[icol]
+                    scol = [header.get_caption(), sub_header.get_caption()]
+                    for val in array[:, icol]:
+                        scol.append(fmt % val)
+
+                    scols.append(scol)
+
+        for scol in scols:
+            width = max(len(s) for s in scol)
+            for i in range(len(scol)):
+                scol[i] = scol[i].rjust(width)
+
+        return '\n'.join(' '.join(s for s in srow) for srow in zip(*scols))
+
+    def add_computed_col(self, header, func):
+        header = anything_to_header(header)
+        self.add_recipe(SimpleRecipe(header, func))
 
 
 class Recipe(object):
@@ -232,12 +340,25 @@ class Recipe(object):
         self._required_headers = []
         self._headers = []
         self._col_update_map = {}
+        self._name_to_header = {}
+
+    def has_col(self, name):
+        return name in self._name_to_header
 
     def get_col_names(self):
-        return [h.name for h in self._headers]
+        names = []
+        for h in self._headers:
+            names.append(h.name)
+            for sh in h.sub_headers:
+                names.append(sh.name)
+
+        return names
 
     def get_table(self):
         return self._table
+
+    def get_header(self, name):
+        return self._name_to_header[name]
 
     def _add_required_cols(self, table):
         for h in self._headers:
@@ -246,18 +367,39 @@ class Recipe(object):
 
     def _update_col(self, table, name):
         if not self._table.has_col(name):
+            print('aufruf')
             self._col_update_map[name](table)
 
     def _add_rows_handler(self, table, nrows_added):
         pass
 
-    def _register_required_cols(self, headers):
-        self._required_headers.extend(headers)
+    def _register_required_col(self, header):
+        self._required_headers.append(header)
 
-    def _register_computed_cols(self, headers, updater):
-        self._headers.extend(headers)
-        for h in headers:
-            self._col_update_map[h.name] = updater
+    def _register_computed_col(self, header, updater):
+        self._headers.append(header)
+        self._name_to_header[header.name] = header
+        self._col_update_map[header.name] = updater
+        for sh in header.sub_headers:
+            self._col_update_map[sh.name] = updater
+            self._name_to_header[sh.name] = sh
+
+
+class SimpleRecipe(Recipe):
+
+    def __init__(self, header, func):
+        Recipe.__init__(self)
+        self._col_name = header.name
+
+        def call_func(tab):
+            self._table.add_col(header, func(tab))
+
+        self._register_computed_col(header, call_func)
+
+    def _add_rows_handler(self, table, nrows_added):
+        Recipe._add_rows_handler(self, table, nrows_added)
+        if self._table.has_col(self._col_name):
+            self._table.remove_col(self._col_name)
 
 
 class LocationRecipe(Recipe):
@@ -265,25 +407,23 @@ class LocationRecipe(Recipe):
     def __init__(self):
         Recipe.__init__(self)
 
-        self._register_required_cols(
-            headers=[
-                Header(name='c5', sub_headers=[
-                    Header(name='ref_lat', unit='degrees'),
-                    Header(name='ref_lon', unit='degrees'),
-                    Header(name='north_shift', unit='m'),
-                    Header(name='east_shift', unit='m'),
-                    Header(name='depth', unit='m')])])
+        self._register_required_col(
+            Header(name='c5', sub_headers=[
+                SubHeader(name='ref_lat', unit='degrees'),
+                SubHeader(name='ref_lon', unit='degrees'),
+                SubHeader(name='north_shift', unit='m'),
+                SubHeader(name='east_shift', unit='m'),
+                SubHeader(name='depth', unit='m')]))
 
-        self._register_computed_cols(
-            headers=[
-                Header(name='latlon', sub_headers=[
-                    Header(name='lat', unit='degrees'),
-                    Header(name='lon', unit='degrees')])],
-            updater=self._update_latlon)
+        self._latlon_header = Header(name='latlon', sub_headers=[
+            SubHeader(name='lat', unit='degrees'),
+            SubHeader(name='lon', unit='degrees')])
+
+        self._register_computed_col(self._latlon_header, self._update_latlon)
 
     def _add_rows_handler(self, table, nrows_added):
         Recipe._add_rows_handler(self, table, nrows_added)
-        if self._table.has_col_group('latlon'):
+        if self._table.has_col('latlon'):
             self._table.remove_col('latlon')
 
     def _update_latlon(self, table):
@@ -297,18 +437,12 @@ class LocationRecipe(Recipe):
         latlons[:, 0] = lats
         latlons[:, 1] = lons
 
-        self._table.add_cols(
-            self.__headers,
-            [latlons],
-            self.__group_headers)
+        self._table.add_col(self._latlon_header, latlons)
 
 
 class EventRecipe(LocationRecipe):
 
     def __init__(self):
         LocationRecipe.__init__(self)
-
-        self._register_required_cols(
-            headers=[
-                Header(name='time', unit='s'),
-                Header(name='magnitude')])
+        self._register_required_col(Header(name='time', unit='s'))
+        self._register_required_col(Header(name='magnitude'))
